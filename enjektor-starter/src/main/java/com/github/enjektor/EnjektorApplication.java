ecd6e32827e031d40bc813d288a4b140642fc2bb
enesusta <enesusta@email.com>
2021-01-01 17:05:56 +0300

    Increase version of enjektor-context

--- enjektor-starter/src/main/java/com/github/enjektor/EnjektorApplication.java
+++ enjektor-starter/src/main/java/com/github/enjektor/EnjektorApplication.java
@@ -2,14 +2,10 @@ package com.github.enjektor;
 
 import com.github.enjektor.context.ApplicationContext;
 import com.github.enjektor.context.ApplicationContextImpl;
-import com.github.enjektor.repo.A;
 
 public class EnjektorApplication {
 
     public static void main(String[] args) throws InstantiationException, IllegalAccessException {
         ApplicationContext applicationContext = ApplicationContextImpl.getInstance(EnjektorApplication.class);
-        final A bean = applicationContext.getBean(A.class, "a3");
-        final String a = bean.a();
-        System.out.println("a = " + a);
     }
 }
