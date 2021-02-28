e5ebfbbea6817c79f3fd73e44e487bed0abb6203
enesusta <enesusta@email.com>
2021-02-28 13:05:05 +0300

    Add exceptions, change version of some modules

---
+++ enjektor-core/src/main/java/com/github/enjektor/core/exceptions/InvalidRouterNamingException.java
@@ -0,0 +1,7 @@
+package com.github.enjektor.core.exceptions;
+
+public class InvalidRouterNamingException extends RuntimeException {
+    public InvalidRouterNamingException(String msg) {
+        super(msg);
+    }
+}
